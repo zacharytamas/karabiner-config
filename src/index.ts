7add6c3113186e4217c96c20f16f6edfe2778a93
Zachary Jones <zachary@jones.cx>
2024-04-19 09:25:51 -0400

    Add some initial app launching basics

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,25 @@ import {
   writeToProfile,
   hyperLayer,
   toApp,
+  mapSimultaneous,
 } from 'karabiner.ts'
 
 writeToProfile('Default', [
   rule('Caps Lock → Hyper').manipulators([
-    map('caps_lock').toHyper().toIfAlone('escape'),
+    map('caps_lock')
+      .parameters({ 'basic.to_if_alone_timeout_milliseconds': 250 })
+      .toHyper()
+      .toIfAlone('escape'),
   ]),
+
+  rule('Delete forward')
+    .description('If equals and backspace pressed together, delete forward.')
+    .manipulators([mapSimultaneous(['equal_sign', 'delete_or_backspace']).to('delete_forward')]),
+
   hyperLayer('o', 'hyper-o').manipulators({
     t: toApp('Warp'),
-    v: toApp('Visual Studio Code - Insiders')
+    v: toApp('Visual Studio Code - Insiders'),
+    s: toApp('Spotify'),
+    m: toApp('Messages'),
   }),
 ])
